c15d65676f48bd3f7990dea2d81929e9f870620c
朱明灿 <zhumingcan@zhumingcandeiMac.local>
2017-02-21 17:23:07 +0800

    shimmer

--- Shimmer/ViewController.m
+++ Shimmer/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "FBShimmeringView.h"
 @interface ViewController ()
 
 @end
@@ -17,8 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    FBShimmeringView * shimmerView = [[FBShimmeringView alloc]initWithFrame:self.view.frame];
+    [self.view addSubview:shimmerView];
+    UILabel * label = [[UILabel alloc]initWithFrame:shimmerView.bounds];
+    label.text = @"Shimmer";
+    label.font = [UIFont systemFontOfSize:36.0];
+    label.textAlignment = NSTextAlignmentCenter;
+    shimmerView.contentView = label;
+    shimmerView.shimmering = YES;
+    
+   
 }
-
+-(void)clickButton:(UIButton *)button{
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
